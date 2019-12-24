6e7e3fe7eb33383457fd5d592a4809a6601e0b0e
sam38124 <sam38124@gmail.com>
2019-12-24 13:54:41 +0800

    1224

---
+++ app/src/main/java/com/orange/jzframework/Frag/Frag_Sec.kt
@@ -0,0 +1,12 @@
+package com.orange.jzframework.Frag
+
+import com.orange.jzchi.jzframework.RootFragement
+import com.orange.jzframework.R
+
+class Frag_Sec():RootFragement(R.layout.sec){
+
+    override fun ViewInit() {
+
+    }
+
+}
--- app/src/main/java/com/orange/jzframework/MainActivity.kt
+++ app/src/main/java/com/orange/jzframework/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.orange.jzframework
+import android.view.KeyEvent
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.orange.jzchi.jzframework.RootActivity
+import com.orange.jzframework.Frag.Frag_Home
+class MainActivity : RootActivity() {
+    /*初次載入的代碼處理*/
+    override fun ViewInit(rootview: View) {
+        //設定首頁
+        SetHome(Frag_Home(), "Frag_Home")
+    }
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+    /*頁面切換監聽*/
+    override fun ChangePageListener(tag: String, frag: Fragment) {
+        //SetHome完會返回"tag","Frag_Home()"的值
+    }
 
-class MainActivity : AppCompatActivity() {
+    /*按鈕事件監聽*/
+    override fun KeyLinsten(event: KeyEvent) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
