24417c11204028606dad2c728c5096fbabb5bf06
sam38124 <sam38124@gmail.com>
2020-03-09 01:04:41 -0700

    v7.1

--- JzframeWork/src/main/java/com/orange/jzchi/jzframework/JzActivity.kt
+++ JzframeWork/src/main/java/com/orange/jzchi/jzframework/JzActivity.kt
@@ -142,8 +142,9 @@ abstract class JzActivity : AppCompatActivity(),
                 android.os.Process.killProcess(android.os.Process.myPid())
             }
 
-            override fun checkUpdate(bun:String,a:Boolean):String?{
+            override fun checkUpdate(a:Boolean):String?{
                 val versionChecker = VersionCheck()
+                versionChecker.packagename=applicationContext.packageName
                 try {
                     if(!a){return versionChecker.execute().get()}
                     val mLatestVersionName = versionChecker.execute().get()
@@ -153,7 +154,7 @@ abstract class JzActivity : AppCompatActivity(),
                     ) {
                         val intent = Intent(Intent.ACTION_VIEW)
                         intent.data =
-                            Uri.parse("https://play.google.com/store/apps/details?id=$bun")
+                            Uri.parse("https://play.google.com/store/apps/details?id=${applicationContext.packageName}")
                         startActivity(intent)
 
                     }
--- JzframeWork/src/main/java/com/orange/jzchi/jzframework/tool/VersionCheck.java
+++ JzframeWork/src/main/java/com/orange/jzchi/jzframework/tool/VersionCheck.java
@@ -12,14 +12,14 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class VersionCheck extends AsyncTask<String, String, String> {
-
     private String newVersion;
+    public String packagename="";
 
     @Override
     protected String doInBackground(String... params) {
 
         try {
-            Document document = Jsoup.connect("https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "&hl=en")
+            Document document = Jsoup.connect("https://play.google.com/store/apps/details?id=" + packagename+ "&hl=en")
                     .timeout(30000)
                     .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
                     .referrer("http://www.google.com")
